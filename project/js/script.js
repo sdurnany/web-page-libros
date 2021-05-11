8a4b1f03f7da2a432fc0bc52070d6d21f03ce007
Simón Durán <sdurany@fen.uchile.cl>
2021-05-11 19:48:24 -0400

    slider/cascada

---
+++ project/js/script.js
@@ -0,0 +1,15 @@
+// $("#slider1_icon_right").attr("onClick").alert("holaa")
+
+function cambia_der(){
+    alert("a la derecha")
+}
+function cambia_izq(){
+    alert("a la izquierda")
+}
+$(document).ready(function(){
+    $('.slider').bxSlider({
+      auto: true,
+      pause: 2000,
+      speed: 1200
+    });
+  });
