c74d091d53fa7dd1da2a535fd7398f7fb6e0b687
Simón Durán <sdurany@fen.uchile.cl>
2021-06-09 10:26:08 -0400

    incorporación página categorías y catálogo

--- project/js/script.js
+++ project/js/script.js
@@ -1,11 +1,4 @@
-// $("#slider1_icon_right").attr("onClick").alert("holaa")
 
-function cambia_der(){
-    alert("a la derecha")
-}
-function cambia_izq(){
-    alert("a la izquierda")
-}
 $(document).ready(function(){
     $('.slider').bxSlider({
       auto: true,
